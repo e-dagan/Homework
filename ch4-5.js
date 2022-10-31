5786719dff09fbc5df27d4e350b21b4df017213b
Elena Dagan <82137916+e-dagan@users.noreply.github.com>
2022-10-31 17:58:15 -0400

    change title

--- ch4-5.js
+++ ch4-5.js
@@ -18,32 +18,31 @@
 // Chapter 5
 
 
-const firstName1 = prompt("What is your first name?")
-const lastName1 = prompt("What is your last name?")
+// const firstName1 = prompt("What is your first name?")
+// const lastName1 = prompt("What is your last name?")
 
 
-function sayHello(firstName1, lastName1) {
+// function sayHello(firstName1, lastName1) {
 
-    const message2 = `Hello, ${firstName1} ${lastName1}!`;
-    return message2;
+//     const message2 = `Hello, ${firstName1} ${lastName1}!`;
+//     return message2;
+//     }
+
+//     console.log(sayHello(firstName1, lastName1));
+
+
+
+
+
+function newMin(num1, num2){
+    if num1 > num2{
+        return num2
     }
+    else{
+        return num1
+    }
+}
 
-    
-
-sayHello();
-
-
-
-
-// function newMin(num1, num2){
-//     if num1 > num2{
-//         return num2
-//     }
-//     else{
-//         return num1
-//     }
-// }
-
-// console.log(newMin(4.5, 5)); // Must show 4.5
-// console.log(newMin(19, 9)); // Must show 9
-// console.log(newMin(1, 1));
+console.log(newMin(4.5, 5)); // Must show 4.5
+console.log(newMin(19, 9)); // Must show 9
+console.log(newMin(1, 1));
